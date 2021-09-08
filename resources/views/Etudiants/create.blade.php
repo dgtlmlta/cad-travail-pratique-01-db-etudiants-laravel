f6966a9c596fa58b0cf311ebe2e4510e78fe2c8a
Nicolas Rouleau <nicolas@digitalmilitia.net>
2021-09-08 11:01:27 -0400

    Intégration formulaire étudiant

--- resources/views/Etudiants/create.blade.php
+++ resources/views/Etudiants/create.blade.php
@@ -1,3 +1,4 @@
 <x-layouts.mainframe pageTitle="Ajout d'un nouvel étudiant">
-
+	<x-formulaires.etudiant action="ajout">
+	</x-formulaires.etudiant>
 </x-layouts.mainframe>
--- resources/views/components/formulaires/etudiant.blade.php
+++ resources/views/components/formulaires/etudiant.blade.php
@@ -1,3 +1,33 @@
-<div>
-    <!-- Act only according to that maxim whereby you can, at the same time, will that it should become a universal law. - Immanuel Kant -->
-</div>
+<form action="{{ $action }}">
+    <div>
+        <label for="inputNom">Nom :</label>
+        <input type="text" id="inputNom" name="nom">
+    </div>
+
+    <div>
+        <label for="inputAdresse">Adresse :</label>
+        <textarea id="inputAdresse" name="adresse"></textarea>
+    </div>
+
+    <div>
+        <label for="inputCourriel">Courriel :</label>
+        <input type="email" id="inputCourriel" name="courriel">
+    </div>
+    
+    <div>
+        <label for="inputTelephone"></label>
+        <input type="phone" id="inputTelephone" name="telephone">
+    </div>
+
+    <div>
+        <label for="inputDdn"></label>
+        <input type="date" id="inputDdn" name="ddn">
+    </div>
+
+    <div>
+        <label for="selectVille"></label>
+        <select id="selectVille" name="ville">
+            
+        </select>
+    </div>
+</form>
