568c8ed3c31c80581dc3f76f3a789b1b3eb78b1d
Nicolas Rouleau <nicolas@digitalmilitia.net>
2021-10-07 15:29:55 -0400

    Création des modèles pour le contenu des articles, ArticleContent et ArticleTitre pour permettre la traduction

---
+++ database/factories/ArticleTitleFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\ArticleTitle;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ArticleTitleFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = ArticleTitle::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
