bde4d02bdb0e8d2cc5a6ac3c3c8692f8bf51b9e7
Nicolas Rouleau <nicolas@digitalmilitia.net>
2021-10-12 14:41:12 -0400

    Ajout des boutons de suppression et modification

--- resources/views/components/file-listing.blade.php
+++ resources/views/components/file-listing.blade.php
@@ -11,6 +11,24 @@
             <dd>
                 <a href="/files/{{ $file->id }}">{{ $file->title }}</a>
             </dd>
+
+            <div class="file-item-actions">
+                @can('update', $file)
+                    <a class="btn btn-primary"
+                       href="/files/{{ $file->id }}/modifier">{{ ucfirst(__('files/index.updateButton')) }}</a>
+                @endcan
+
+                @can('delete', $file)
+                    <form action="{{ route('files.destroy', $file->id) }}"
+                          method="POST">
+                        @csrf
+                        @method('delete')
+                        <button class="btn btn-danger"
+                                href="/files/{{ $file->id }}"
+                                type="submit">{{ ucfirst(__('files/index.deleteButton')) }}</button>
+                    </form>
+                @endcan
+            </div>
         </div>
     @empty
         <span>{{ __('files/index.noFileMessage') }}</span>
